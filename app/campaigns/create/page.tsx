8d6c18cda085e5ee26c4c38eb5be090be5b029fe
Oussama <bz.oussama@gmail.com>
2024-12-09 22:07:42 +0400

    NestJS succesfull post to NestJS

--- app/campaigns/create/page.tsx
+++ app/campaigns/create/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import CreateCampaignForm from '../forms/CreateCampaignForm';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function CreateCampaign() {
   return (
-    <>
-      <h1>Hi mom</h1>
-    </>
+    <div className="grid grid-cols-2 gap-4 p-4">
+      <Card className="p-4">
+        <CardHeader className="px-0">
+          <CardTitle className="text-left">Campaign Details</CardTitle>
+        </CardHeader>
+        <CreateCampaignForm />
+        <CardContent></CardContent>
+      </Card>
+      <Card className="p-4">
+        <CardHeader className="px-0">
+          <CardTitle className="text-left">Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eveniet
+          eligendi sed voluptatum. Dolorum sunt dignissimos quas, repellat,
+          voluptatum autem dolore aliquid sapiente distinctio explicabo quia
+          commodi neque reprehenderit soluta.
+        </CardContent>
+      </Card>
+    </div>
   );
 }
---
+++ app/server/actions/createCampaignAction.ts
@@ -0,0 +1,23 @@
+'user server';
+
+import { createCampaignSchema } from '@/app/campaigns/forms/CreateCampaignSchema';
+import { fetchApi } from '@/app/utils/api';
+import { z } from 'zod';
+
+export async function createCampaignAction(
+  unsafeData: z.infer<typeof createCampaignSchema>,
+): Promise<{ error: boolean; message?: string } | undefined> {
+  const { success, data } = createCampaignSchema.safeParse(unsafeData);
+  if (!success) {
+    return { error: true };
+  }
+  try {
+    await fetchApi('/campaigns', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log(error);
+    return { error: true, message: 'Error submitting campaign' };
+  }
+}
