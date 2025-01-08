446c4869078f859182b7fdc751c3237aa8c60f21
Oussama <bz.oussama@gmail.com>
2025-01-08 12:56:26 +0400

    add device checkbox to create  campaign

--- app/campaigns/forms/CreateCampaignSchema.tsx
+++ app/campaigns/forms/CreateCampaignSchema.tsx
@@ -1,5 +1,10 @@
 import { z } from 'zod';
 
+const deviceSchema = z.object({
+  id: z.union([z.literal(1), z.literal(2), z.literal(3)]),
+  name: z.enum(['Desktop', 'Mobile', 'Tablet']),
+});
+
 export const createCampaignSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   advertiserId: z.number().int().positive('Invalid Advertiser Id'),
@@ -7,5 +12,6 @@ export const createCampaignSchema = z.object({
   countries: z
     .array(z.number().int().positive())
     .nonempty('At least one country is required'),
+  device: z.array(deviceSchema).nonempty('At least one device is required'),
   isActive: z.boolean(),
 });
