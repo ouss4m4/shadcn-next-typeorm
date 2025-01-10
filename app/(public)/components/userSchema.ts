17a0b87f96c824252eebb7ee7e02bf8894ac0d38
Oussama <bz.oussama@gmail.com>
2025-01-10 22:41:37 +0400

    add register/login pages actions flow

--- app/(public)/components/userSchema.ts
+++ app/(public)/components/userSchema.ts
@@ -1,14 +1,19 @@
 import { z } from 'zod';
 
-export const registerSchema = z.object({
-  username: z.string().min(3).max(20),
-  email: z.string().email('Email must be valid'),
-  password: z.string().min(6).max(100),
-  password2: z
-    .string()
-    .min(8, 'Password length must contain at least 8 characters')
-    .max(100),
-});
+export const registerSchema = z
+  .object({
+    name: z.string().min(3).max(20),
+    email: z.string().email('Email must be valid'),
+    password: z.string().min(6).max(100),
+    password2: z
+      .string()
+      .min(8, 'Password length must contain at least 8 characters')
+      .max(100),
+  })
+  .refine((data) => data.password === data.password2, {
+    message: 'Passwords must match',
+    path: ['password2'],
+  });
 
 export const loginSchema = z.object({
   email: z.string().email('Email must be valid'),
